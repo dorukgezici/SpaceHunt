24597f76b5b6f916eb991395e5566c1109de88f1
Wladimir Hofmann <wladimir.hofmann@student.tut.fi>
2018-04-02 13:49:42 +0300

    suppressed player movement in level 4 while jumping

--- code/src/Scenes/Level4/Player.ts
+++ code/src/Scenes/Level4/Player.ts
@@ -7,6 +7,7 @@ export default class Player extends BasePlayer {
 	private minX: number;
 	private maxX: number;
 	dead: boolean = false;
+	private moveDir: number = 0;
 
 	constructor(x: number, y: number, levelBounds: ex.BoundingBox) {
 		super(x, y);
@@ -21,24 +22,34 @@ export default class Player extends BasePlayer {
 	update(engine: ex.Engine, delta: number) {
 		super.update(engine, delta);
 
-		if (engine.input.keyboard.wasPressed(ex.Input.Keys.Space)) {
-			this.jump();
+		// change direction of movement if not currently jumping
+		if (this.isGround()) {
+
+			if (engine.input.keyboard.wasPressed(ex.Input.Keys.Space)) {
+				this.jump();
+			}
+
+			this.moveDir = 0;
+			if (engine.input.keyboard.isHeld(ex.Input.Keys.Left)) {
+				// this.goLeft();
+				this.moveDir = 1;
+			}
+
+			if (engine.input.keyboard.isHeld(ex.Input.Keys.Right)) {
+				// this.goRight();
+				this.moveDir = -1;
+			}
 		}
 
-		if (engine.input.keyboard.isHeld(ex.Input.Keys.Left)) {
-			this.goLeft();
-		}
-
-		if (engine.input.keyboard.isHeld(ex.Input.Keys.Right)) {
-			this.goRight();
-		}
+		// move according to direction of movement
+		this.pos.x -= Player.speed * this.moveDir;
+		this.pos.x = this.pos.x < this.minX ? this.minX : this.pos.x;
+		this.pos.x = this.pos.x > this.maxX ? this.maxX : this.pos.x;
 
 	}
 
 	private jump() {
-		let groundLevel = this.scene.engine.getWorldBounds().bottom - 50;
-
-		if (groundLevel - this.getBottom() < 15) {
+		if (this.isGround()) {
 			this.vel.setTo(this.vel.x, -700);
 			// console.log(this.vel);
 		}
@@ -64,10 +75,10 @@ export default class Player extends BasePlayer {
 			// this.setHeight(this.getHeight() / 4);
 			this.collisionArea.body.useBoxCollision();
 
-			this.scene.camera.shake(50, 50, 500);			
+			this.scene.camera.shake(50, 50, 500);
 
 			let player: Player = this;
-			setTimeout(function() {
+			setTimeout(function () {
 
 				player.kill();
 				alert(info);
@@ -76,4 +87,12 @@ export default class Player extends BasePlayer {
 		}
 	}
 
+	private isGround(): boolean {
+		let groundLevel = this.scene.engine.getWorldBounds().bottom - 50;
+		if (groundLevel - this.getBottom() < 15) {
+			return true;
+		} else {
+			return false;
+		}
+	}
 }
