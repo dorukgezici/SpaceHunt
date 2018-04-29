2f01aa9b854a41e18e9ec87e1df8356ce03017a7
Nejc Maček <nejc.macek@student.tut.fi>
2018-04-29 16:20:09 +0300

    Fixed vine unit tests

--- test/Level1/PlayerVine.ts
+++ test/Level1/PlayerVine.ts
@@ -17,11 +17,21 @@ describe("Player and Vine interaction", () => {
 		names: ["Freddy"]
 	};
 
-	const engine = new ex.Engine();
-	const scene = new ex.Scene(engine);
-	const vine = new Vine(200, 0, 20, 1.0, 0.7);
+	const data = {
+		// @ts-ignore
+		engine: null as ex.Engine,
+		// @ts-ignore
+		scene: null as ex.Scene,
+		// @ts-ignore
+		vine: null as Vine
+	};
 
-	scene.add(vine);
+	beforeEach(() => {
+		data.engine = new ex.Engine();
+		data.scene = new ex.Scene(data.engine);
+		data.vine = new Vine(200, 0, 20, 1.0, 0.7);
+		data.scene.add(data.vine);
+	});
 
 	it("Should not be affected before colliding", () => {
 		let player = new Level1Player(0, 100, 2000, controlSets.controls1, state, true);
@@ -31,29 +41,31 @@ describe("Player and Vine interaction", () => {
 		expect(player.onVine).toBeFalsy();
 		expect(player.collisionType).toEqual(ex.CollisionType.Active);
 
-		vine.getAllParts().map(x => {
+		data.vine.getAllParts().map(x => {
 			expect(x.x).toEqual(200);
 		});
 	});
 
 	it("Should attach to vine on collision", () => {
 		let player = new Level1Player(199, 100, 2000, controlSets.controls1, state, true);
-		scene.add(player);
+		data.scene.add(player);
 
 		player.emit("precollision",
-			new ex.PreCollisionEvent(player, vine.getAllParts()[5], ex.Side.Right, new ex.Vector(10, 10)));
+			new ex.PreCollisionEvent(player, data.vine.getAllParts()[5], ex.Side.Right, new ex.Vector(10, 10)));
 
 		expect(player.onVine).toBeTruthy();
 		expect(player.parent.constructor.name).toEqual("Vine");
-		expect(vine.alreadyCollidedWith[0]).toEqual(player);
+		console.log(data.vine.alreadyCollidedWith[0] === player);
+		console.log(data.vine.alreadyCollidedWith.length);
+		expect(data.vine.alreadyCollidedWith[0] === player).toBeTruthy();
 		expect(player.x).toEqual(0);
 		expect(player.getWorldPos().x).toEqual(200);
 	});
 
 	it("Should detach after jump", () => {
 		let player = new Level1Player(199, 100, 2000, controlSets.controls1, state, true);
-		scene.add(player);
-		player.attachToVine(vine.getAllParts()[5]);
+		data.scene.add(player);
+		player.attachToVine(data.vine.getAllParts()[5]);
 
 		expect(player.onVine).toBeTruthy();
 
@@ -64,7 +76,8 @@ describe("Player and Vine interaction", () => {
 		expect(player.parent).toBeFalsy();
 		expect(player.vel.x).toEqual(600);
 		expect(player.vel.y).toEqual(-500);
-		expect(player.x).toEqual(vine.getAllParts()[5].x);
-		expect(player.getWorldPos().x).toEqual(vine.getAllParts()[5].x);
+		expect(player.x).toEqual(data.vine.getAllParts()[5].x);
+		expect(player.getWorldPos().x).toEqual(data.vine.getAllParts()[5].x);
 	});
+
 });
